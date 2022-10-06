8ff5c5cd7633957bdbaf8e3aaf9e9f9ab6667490
Stellarthoughts <49097167+Stellarthoughts@users.noreply.github.com>
2022-10-06 23:34:03 +0700

    Front-end init

---
+++ ui/src/theme.ts
@@ -0,0 +1,20 @@
+import { Theme } from "@mui/material/styles";
+import { createTheme } from '@mui/material/styles';
+
+declare module '@mui/material/styles' {
+  interface Theme {
+    status: {
+      danger: string;
+    };
+  }
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+    status?: {
+      danger?: string;
+    };
+  }
+}
+
+const theme = createTheme();
+
+export default theme;
