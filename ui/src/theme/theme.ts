ee4abfa11098047692444398df1104d9a7cad6b3
Stellarthoughts <49097167+Stellarthoughts@users.noreply.github.com>
2022-11-05 18:02:18 +0700

    fix(formatting): ident

--- ui/src/theme/theme.ts
+++ ui/src/theme/theme.ts
@@ -2,17 +2,17 @@ import { Theme } from "@mui/material/styles";
 import { createTheme } from '@mui/material/styles';
 
 declare module '@mui/material/styles' {
-  interface Theme {
-    status: {
-      danger: string;
-    };
-  }
-  // allow configuration using `createTheme`
-  interface ThemeOptions {
-    status?: {
-      danger?: string;
-    };
-  }
+	interface Theme {
+		status: {
+			danger: string;
+		};
+	}
+	// allow configuration using `createTheme`
+	interface ThemeOptions {
+		status?: {
+			danger?: string;
+		};
+	}
 }
 
 const theme = createTheme();
