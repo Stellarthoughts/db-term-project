780c63677ce5b561f8c62b12e341e1724ce67442
Stellarthoughts <49097167+Stellarthoughts@users.noreply.github.com>
2022-12-09 19:45:35 +0700

    fix(ui): upload

--- ui/src/components/pages/uploadPage.tsx
+++ ui/src/components/pages/uploadPage.tsx
@@ -2,12 +2,42 @@ import Box from "@mui/material/Box"
 import Typography from "@mui/material/Typography"
 import { MuiFileInput } from "mui-file-input"
 import { useState } from "react"
+import { GenRequest } from "../../request/general"
+import axios from 'axios';
+import Button from "@mui/material/Button"
 
 function UploadPage() {
 	const [file, setFile] = useState<File | null>(null)
 
 	const handleChange = (value: File | null) => {
 		setFile(value)
+		console.log(value)
+
+	}
+
+	const handleOnClick = (e: any) => {
+		/* const configInit = {
+			method: "POST",
+			headers: {
+				'Content-Type': 'multipart/form-data',
+				'X-Access-Token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjYsImxvZ2luIjoibG9nMzJpMW4iLCJpYXQiOjE2NzA1NzU4OTYsImV4cCI6MTY3MDU4MzA5Nn0.QHFw2v1ny_vnkQvbARvDuyAEjDSQ5PPeAyohFTzwrkE'
+			},
+			files: file
+		}
+		const request = "/api/upload"
+		GenRequest(request, configInit) */
+		if (file == null)
+			return
+
+		const formData = new FormData();
+
+		formData.append(
+			"file",
+			file as Blob,
+			file.name
+		)
+		// Update the formData object
+		axios.post("/api/upload", formData);
 	}
 
 	return (
@@ -23,7 +53,8 @@ function UploadPage() {
 				Hello!
 			</Typography>
 			<MuiFileInput value={file} onChange={handleChange} />
-		</Box>
+			<Button onClick={handleOnClick} variant="contained">Send</Button>
+		</Box >
 	)
 }
 
